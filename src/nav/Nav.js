5836a7e69d2f140cdfbc9add876d9c8560137d12
momo1108 <38344292+momo1108@users.noreply.github.com>
2019-08-18 22:08:32 +0900

    index-Navbar

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -13,10 +13,16 @@ class Nav extends Component {
         return(
             <div className = "Navbar">
                 <div className = "Navlogo">
-                    <img src="logo.png" width="100px"/>
+                    <img src="logo.png" width="130px" height="40.44px"/>
                 </div>
                 <div className = "Navmenu">
-                    b
+                    <div className = "Menubtn">메뉴&nbsp;&nbsp;<i class="fas fa-chevron-right"></i></div>
+                    <div className = "Menubtn">이벤트&nbsp;&nbsp;<i class="fas fa-chevron-right"></i></div>
+                    <div className = "Signup">회원가입</div>
+                    <div className = "Login">로그인</div>{/* 핑크 상자안에 넣어놓음 */}
+                </div>
+                <div className = "Navsmallmenu">
+                    <i className="fas fa-bars"></i>
                 </div>
             </div>
         )
